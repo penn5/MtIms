4cd95f6f9b59b295aabb05a0100a2ae1c19d90fb
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-05-13 14:07:25 +0100

    Add debug and more good stuff to ImsConfig

--- app/src/main/java/com/mediatek/ims/MtImsConfig.kt
+++ app/src/main/java/com/mediatek/ims/MtImsConfig.kt
@@ -1,11 +1,14 @@
 package com.mediatek.ims
 
 import android.annotation.NonNull
+import android.hardware.radio.V1_0.RadioResponseInfo
+import android.telephony.Rlog
 import android.telephony.ims.stub.ImsConfigImplBase
+import android.util.Log
 import com.android.ims.ImsConfig
 import java.util.concurrent.ConcurrentHashMap
 
-class MtImsConfig(val mSlotId: Int) : ImsConfigImplBase() {
+class MtImsConfig(private val mSlotId: Int) : ImsConfigImplBase() {
     private val configInt = ConcurrentHashMap<Int, Int>()
     private val configString = ConcurrentHashMap<Int, String>()
 
@@ -18,22 +21,39 @@ class MtImsConfig(val mSlotId: Int) : ImsConfigImplBase() {
 
     override fun setConfig(item: Int, value: Int): Int {
         configInt[item] = value
-        notifyProvisionedValueChanged(item, value)
+        Log.d(tag, "Setting config int $item=$value")
+        if (item == ImsConfig.ConfigConstants.VOLTE_USER_OPT_IN_STATUS)
+            RilHolder.getRadio(mSlotId).setImsVoiceEnable(RilHolder.callback({ radioResponseInfo: RadioResponseInfo, data: Array<out Any?> ->
+                if (radioResponseInfo.error != 0) {
+                    Rlog.e(tag, "Failed to setImsVoiceEnable to user optin status, $radioResponseInfo $data")
+                } else {
+                    Rlog.d(tag, "setImsVoiceEnable success for user optin status")
+                    notifyProvisionedValueChanged(item, value)
+                }
+            }, mSlotId), value > 0)
+        else
+            notifyProvisionedValueChanged(item, value)
         return ImsConfig.OperationStatusConstants.SUCCESS
     }
 
     override fun setConfig(item: Int, value: String): Int {
         configString[item] = value
+        Log.d(tag, "Setting config string $item=$value")
         notifyProvisionedValueChanged(item, value)
         return ImsConfig.OperationStatusConstants.SUCCESS
     }
 
     override fun getConfigInt(@NonNull item: Int): Int {
+        Log.d(tag, "Returning config int $item=${configInt[item]}")
         return configInt[item] ?: ImsConfig.FeatureValueConstants.ERROR
-
     }
 
     override fun getConfigString(item: Int): String? {
+        Log.d(tag, "Returning config string $item=${configString[item]}")
         return configString[item]
+    }
+
+    companion object {
+        const val tag = "MtImsConfig"
     }
 }
