c3d4d7ff8b826dfa2807d6596870b007b094bfa8
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-05-17 19:11:01 +0100

    set VDP correctly Use reverse engineered constants: 1=CS only 2=CS priority 3=PS priority 4=PS only

--- app/src/main/java/com/mediatek/ims/MtImsConfig.kt
+++ app/src/main/java/com/mediatek/ims/MtImsConfig.kt
@@ -17,15 +17,6 @@ class MtImsConfig(private val mSlotId: Int) : ImsConfigImplBase() {
         Log.d(tag, "Setting config int $item=$value")
         when (item) {
             ImsConfig.ConfigConstants.VLT_SETTING_ENABLED -> {
-                RilHolder.getRadio(mSlotId).setConfig(-1, ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_LTE, value.toString())
-                RilHolder.getRadio(mSlotId)
-                    .setImsVoiceEnable(RilHolder.callback({ radioResponseInfo: RadioResponseInfo, data: Array<out Any?> ->
-                        if (radioResponseInfo.error != 0) {
-                            Rlog.e(tag, "Failed to setImsVoiceEnable to user optin status, $radioResponseInfo $data")
-                        } else {
-                            Rlog.d(tag, "setImsVoiceEnable success for user optin status")
-                        }
-                    }, mSlotId), value > 0)
                 RilHolder.getRadio(mSlotId)
                     .setVolteEnable(RilHolder.callback({ radioResponseInfo: RadioResponseInfo, data: Array<out Any?> ->
                         if (radioResponseInfo.error != 0) {
@@ -33,21 +24,38 @@ class MtImsConfig(private val mSlotId: Int) : ImsConfigImplBase() {
                         } else {
                             Rlog.d(tag, "setVolteEnable success for user optin status")
                         }
-
-                        RilHolder.getRadio(mSlotId).setVoiceDomainPreference(
-                            RilHolder.callback({ radioResponseInfo2: RadioResponseInfo, data2: Array<out Any?> ->
+                        RilHolder.getRadio(mSlotId)
+                            .setImsVoiceEnable(RilHolder.callback({ radioResponseInfo2: RadioResponseInfo, data2: Array<out Any?> ->
                                 if (radioResponseInfo2.error != 0) {
-                                    Rlog.e(tag, "Failed to setVoiceDomainPreference, $radioResponseInfo2 $data2")
+                                    Rlog.e(
+                                        tag,
+                                        "Failed to setImsVoiceEnable to user optin status, $radioResponseInfo2 $data2"
+                                    )
                                 } else {
-                                    Rlog.d(tag, "setVoiceDomainPreference success yay")
+                                    Rlog.d(tag, "setImsVoiceEnable success for user optin status")
                                     notifyProvisionedValueChanged(item, value)
                                 }
-                            }, mSlotId), value + 1
-                        ) // User opt in status is 0 (false/CS) or 1 (true/PS) but android.telephony.NetworkRegistrationState says we should use 1 (false/CS) or 2 (true/PS)
+                            }, mSlotId), value > 0)
                     }, mSlotId), value > 0)
             }
             ImsConfig.ConfigConstants.VOLTE_USER_OPT_IN_STATUS -> {
-                RilHolder.getRadio(mSlotId).setConfig(-1,ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_LTE, value.toString())
+                RilHolder.getRadio(mSlotId)
+                    .setVoiceDomainPreference(
+                        RilHolder.callback({ radioResponseInfo: RadioResponseInfo, data: Array<out Any?> ->
+                            if (radioResponseInfo.error != 0) {
+                                Rlog.e(
+                                    tag,
+                                    "Failed to set vdp to user preference! VoLTE won't be used! $radioResponseInfo $data"
+                                )
+                            } else {
+                                Rlog.d(tag, "Success to set vdp! :D")
+                                notifyProvisionedValueChanged(item, value)
+                            }
+                        }, mSlotId), when (value) {
+                            0 -> 2 // CS priority, PS fallback
+                            else -> 3 // PS priority, CS fallback
+                        }
+                    )
             }
             else -> notifyProvisionedValueChanged(item, value)
         }
@@ -56,7 +64,6 @@ class MtImsConfig(private val mSlotId: Int) : ImsConfigImplBase() {
 
     override fun setConfig(item: Int, value: String): Int {
         configString[item] = value
-        RilHolder.getRadio(mSlotId).setConfig(-1, item, value)
         Log.d(tag, "Setting config string $item=$value")
         notifyProvisionedValueChanged(item, value)
         return ImsConfig.OperationStatusConstants.SUCCESS
